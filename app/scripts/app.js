393e3e0b927fdfa914ff3d215a0e214ec1f2e6c1
bylertall <tball88@gmail.com>
2015-03-31 21:57:26 -0700

    add new room functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('blabby', ['ui.router', 'firebase']);
+var app = angular.module('blabby', ['ui.router', 'firebase', 'ui.bootstrap']);
 
 app.config(['$stateProvider', function($stateProvider) {
 
@@ -17,12 +17,45 @@ app.config(['$stateProvider', function($stateProvider) {
 
 /*Controllers*/
 
-app.controller('HomeCtrl', ['Room', function(Room) {
+app.controller('HomeCtrl', ['Room', '$modal', function(Room, $modal) {
   var vm = this;
 
   vm.title = "Blabby".toUpperCase();
-  vm.roomArray = Room.all;
-  console.log(vm.roomArray);
+
+  //Array of rooms from Firebase
+  vm.rooms = Room.all;
+
+  //Modal to add new room
+  vm.roomModal = function() {
+    var modalInstance = $modal.open({
+      templateUrl: '/templates/room-modal.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: 'modal'
+    });
+
+    //Add room to firebase on modal.ok
+    modalInstance.result.then(function(roomName) {
+      Room.add(roomName);
+    });
+
+  };
+
+}])
+
+app.controller('ModalInstanceCtrl', ['$modalInstance', 'Room', function($modalInstance, Room) {
+  var vm = this;
+
+  vm.roomName = '';
+
+  // Add method to submit modal input for new room
+  vm.ok = function() {
+    $modalInstance.close(vm.roomName);
+  };
+
+  vm.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+
 
 }])
 
@@ -36,9 +69,17 @@ app.factory('Room', ['$firebase', function($firebase){
   var firebaseRef = new Firebase(firebaseAPI);
 
   var rooms = $firebase(firebaseRef.child('rooms')).$asArray();
+
+  var addRoom = function(roomName) {
+    rooms.$add({room: roomName})
+      .then(function() {
+        console.log('The room as been added!');
+    });
+  };
   
   return {
-    all: rooms
+    all: rooms,
+    add: addRoom
   }
   
 }])
